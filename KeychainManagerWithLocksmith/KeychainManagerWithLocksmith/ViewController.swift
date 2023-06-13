214aa8005f51e59bd8a98d45f12c8c9e5a23ba14
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-13 08:54:39 +0300

    Add outlets and action for elements

--- KeychainManagerWithLocksmith/KeychainManagerWithLocksmith/ViewController.swift
+++ KeychainManagerWithLocksmith/KeychainManagerWithLocksmith/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBActions
+    
+    @IBOutlet weak private var loginTextField: UITextField!
+    @IBOutlet weak private var passwordTextField: UITextField!
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: - IBActions
 
-
+    @IBAction private func saveButtonAction(_ sender: UIButton) {
+    }
 }
 
